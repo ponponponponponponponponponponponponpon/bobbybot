654f5e9653419058cbb84018c8bb42e1795807c2
Zachary <zachceniceros@gmail.com>
2023-08-25 13:43:18 -0400

    refactored code to fit GPT4's standards/requirements. bobby bot seems to up and running correctly

--- index.js
+++ index.js
@@ -1,45 +1,104 @@
 // create a discord bot using openai api that interacts on the discord server
 require('dotenv').config();
 
-// prepare to connect to the discord api
-const { Client, GatewayIntentBits } = require('discord.js')
+const { Client, IntentsBitField } = require('discord.js');
+const { OpenAI } = require('openai');
+
 const client = new Client({ intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent
 ]});
 
-// prepare connection to openai api
-const { OpenAI } = require('openai');
+client.on('ready', () => {
+    console.log("bobby is online!");
+})
+
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_KEY // This is also the default, can be omitted
+    apiKey: process.env.OPENAI_KEY,
 });
 
-// check for when a message on discord is sent
-client.on('messageCreate', async function(message){
-    try {
-        // don't respond to yourself bobby!
-        if(message.author.bot) return;
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(':3')) return;
 
-        const gptResponse = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            temperature: 0.9, // temp determines ai randomness - 1 being very random
-            max_tokens: 100,
-            stop: ["ChatGPT:"],
-            messages: [{"role": "user", "content": "hello!"}],
-            // prompt: `ChatGPT is a friendly, shy, and human-like response chatbot. ChatGPT only types in lowercase letters.\n\
-            // ChatGPT: Hello, how are you?\n\
-            // ${message.author.username}: ${message.content}\n\
-            // ChatGPT:`
-        });
+    let conversationLog = [{ role: 'system', content: 'You are a friendly and shy chatbot. Your name is Bobby. Your responses are always in lowercase letters. You also try to make yourself sound less robotic and more like a human.'}]
 
-        message.reply(`${gptResponse.choices[0].message}`);
-        return;
-    } catch(err) {
-        console.log(err);
-    }
-});
+    await message.channel.sendTyping();
 
-// log the bot
+    let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+    prevMessages.reverse();
+
+    prevMessages.forEach((msg) => {
+        if (message.content.startsWith('!')) return;
+        if (msg.author.id !== client.user.id && message.author.bot) return;
+        if (msg.author.id !== message.author.id) return;
+
+        conversationLog.push({
+            role: 'user',
+            content: msg.content,
+        })
+    })
+
+
+    const result = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: conversationLog,
+    })
+
+    message.reply(result.choices[0].message);
+})
+
 client.login(process.env.DISCORD_TOKEN);
-console.log("Pon Bot is Online!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // prepare connection to openai api
+// const { OpenAI } = require('openai');
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_KEY // This is also the default, can be omitted
+// });
+
+// // check for when a message on discord is sent
+// client.on('messageCreate', async function(message){
+//     try {
+//         // don't respond to yourself bobby!
+//         if(message.author.bot) return;
+
+//         const gptResponse = await openai.chat.completions.create({
+//             model: "gpt-3.5-turbo",
+//             temperature: 0.9, // temp determines ai randomness - 1 being very random
+//             max_tokens: 100,
+//             stop: ["ChatGPT:"],
+//             messages: [{"role": "user", "content": "hello!"}],
+//             // prompt: `ChatGPT is a friendly, shy, and human-like response chatbot. ChatGPT only types in lowercase letters.\n\
+//             // ChatGPT: Hello, how are you?\n\
+//             // ${message.author.username}: ${message.content}\n\
+//             // ChatGPT:`
+//         });
+
+//         message.reply(`${gptResponse.choices[0].message}`);
+//         return;
+//     } catch(err) {
+//         console.log(err);
+//     }
+// });
+
+// // log the bot
+// client.login(process.env.DISCORD_TOKEN);
+// console.log("Pon Bot is Online!");
